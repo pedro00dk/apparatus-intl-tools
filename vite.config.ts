e48656f8e9b7db31e1c8c36aed9e021974ef8d3d
Pedro Henrique <pedro00dk@gmail.com>
2025-11-09 17:44:22 +1100

    Update packages and change package manager to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({ build: { lib: { entry: ['./src/index.ts'], formats: ['es'] } } })
+export default defineConfig({
+    build: {
+        lib: { entry: ['./src/index.ts'], formats: ['es'] },
+        minify: false,
+    },
+})
